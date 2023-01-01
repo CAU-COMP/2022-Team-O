9cf7e3d0a029536a65d883a7eb0d8910a01c8184
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-02 02:10:12 +0900

    서버 코드 작성 및 패키지 업데이트

--- Backend/main.js
+++ Backend/main.js
@@ -1,21 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
-    if(_url == '/'){
-        _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    // console.log(__dirname + url);
-    response.end(queryData.id);
- 
+import http from "http";
+import express from "express"; 
+
+const app = express();
+const server = http.createServer(app);
+
+const PORT = 8080; // 아마존 EC2 업로드 시에는 HTTP용으로 80번으로 바꿀 예정
+
+// const server = http.createServer(function(request,response){ 
+//     response.writeHead(200,{'Content-Type':'text/html'});
+//     response.end('Hello node.js!!');
+// }); // HTTP모듈로 만든 서버
+
+// 요청을 수행하는 방법은
+// app.get('요청한주소', function (req, res) {
+//   res.send('res내용');
+// }); // 이런식으로
+
+server.listen(PORT, function(){ 
+    console.log(`Server is running at port ${PORT}`);
 });
-app.listen(3000);

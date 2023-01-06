f6c76f0154b1d98383b3f5452a57dc420eecf805
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-06 18:37:33 +0900

    Update sendEmail.js

--- Backend/sendEmail.js
+++ Backend/sendEmail.js
@@ -1,19 +1,17 @@
-import AWS from 'aws-sdk'
 // import * as keys from "../../SES_Access_Key.json" assert { type: "json" }
+import AWS from 'aws-sdk'
 import fs from "fs"
 
 let keys = '';
 
 fs.readFile("../../SES_Access_Key.json", "utf-8", function (err, data) {
   if (err) throw err;
-  // console.log(data);
   keys = JSON.parse(data);
-  // console.log(keys.accessKey);
-  // console.log(keys.secretAccessKey);
 });
 
-// console.log(keys.default.accessKey);
-// console.log(keys.default.secretAccessKey);
+// console.log(data);
+// console.log(keys.accessKey);
+// console.log(keys.secretAccessKey);
 
 const SES_CONFIG = {
     accessKeyId: keys.accessKey,
@@ -23,25 +21,29 @@ const SES_CONFIG = {
 
 const AWS_SES = new AWS.SES(SES_CONFIG);
 
-let sendEmail = (recipientEmail, recipientName) => {
+let sendEmail = (recipientEmail, bodyContent, mailTitle) => { // Title 에 수신자 이름 포함할 것
     let params = {
-      Source: 'mail@caunotify.me',
+      Source: "mail@caunotify.me",
       Destination: {
         ToAddresses: [
           recipientEmail
         ],
       },
       ReplyToAddresses: [],
-      Message: {
-        Body: {
+      Message: { /* required */
+        Body: { /* required */
           Html: {
-            Charset: 'UTF-8',
-            Data: 'This is the body of my email!',
+          Charset: "UTF-8",
+          Data: "HTML_FORMAT_BODY"
           },
+          Text: {
+          Charset: "UTF-8",
+          Data: "TEXT_FORMAT_BODY"
+          }
         },
         Subject: {
           Charset: 'UTF-8',
-          Data: `Hello, ${recipientName}!`,
+          Data: 'Test email'
         }
       },
     };
@@ -62,9 +64,10 @@ let sendTemplateEmail = (recipientEmail) => {
     return AWS_SES.sendTemplatedEmail(params).promise();
 };
 
-// export default {
-//     sendEmail,
-//     sendTemplateEmail,
-// };
 
-sendEmail("na_sanghyun@naver.com","NaSangHyun");
+sendEmail("na_sanghyun@naver.com","NaSangHyun","MailTitle");
+
+export default {
+    sendEmail,
+    sendTemplateEmail,
+};

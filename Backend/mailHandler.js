5f7608d2bdf53c4e6fe875ecc92d56296f16c8ef
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-07 13:21:33 +0900

    updated encoding and var type

--- Backend/mailHandler.js
+++ Backend/mailHandler.js
@@ -2,7 +2,12 @@ import {sendEmail, sendTemplateEmail} from "./sendEmail.js"
 import fs from "fs"
 
 function mailHandler(){
-    const bodyContent = fs.readFileSync("./Test2.html", "utf8");
+    // 수신자주소, 바디내용, 메일 제목을 받음
+    // 바디 내용에는 어느 게시판을 구독했는지에 따라 추가적인 내용이 들어갈 수 있으므로
+    // 앞부분 스트링을 bodyContent라는 string에 저장하고,
+    // 그 뒷 부분에 추가할 부분을 <a href></a>로 추가하고(join 또는 concat 등등),
+    // 제일 뒤에 </body>랑 </html> 등의 태그를 추가한다.
+    let bodyContent = fs.readFileSync("./Test2.html", "utf8");
     // bodyContent = "TestBodyContent";
     // params : recipientEmail, bodyContent, mailTitle
     sendEmail("na_sanghyun@naver.com",bodyContent,"MailTitle")

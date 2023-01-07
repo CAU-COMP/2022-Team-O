5671c0b14b176cdfd887eb952a91954482fd4138
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-07 23:24:20 +0900

    함수 내 단계 분할 및 필요 기능 추가

--- Backend/refresh.js
+++ Backend/refresh.js
@@ -7,52 +7,81 @@ import crawlKorean from "./url_scraper_korean.js";
 import { compareTwoArrays } from "./compare.js"
 import { mailHandler } from "./mailHandler.js";
 
+// 새 게시판 추가될 때 마다 수정할 것
+const numberOfMajors = 5;
+
+
 // refresh 함수에서는
 // 크롤러 실행 -> 기존 목록과 대조 -> 변화 있으면 sendMail() 후 원래 목록 대체, 없으면 행동하지 않음 -> 다음 크롤러 실행.
-export async function refresh(){
-    const new_industSec = await crawlIndustSec("url"); // 이 반환값에 .title 또는 .url을 이용해 값에 접근할 수 있음
+export async function refresh(nextIdNum){
+    // *********************************
+    // *** 1. 크롤러로부터 데이터 로드 ***
+    // *********************************
+    const new_industSec = await crawlIndustSec("url");
+    console.log("industSec loaded");
     const new_software = await crawlSoftware("url");
+    console.log("software loaded");
     const new_CAUnotice = await crawlCAUnotice("url");
+    console.log("CAUnotice loaded");
     const new_integEngineering = await crawlIntegEngineering("url");
+    console.log("integEngineering loaded");
     const new_korean = await crawlKorean("url");
-    let industSecUpdates;
-    let softwareUpdates;
-    let CAUnoticeUpdates;
-    let integEngineeringUpdates;
-    let koreanUpdates;
+    console.log("korean loaded");
+    
+    
+    // *****************************************************************
+    // *** 2. 기존 목록을 읽어 현재 데이터와 비교한 후, 변경 개수를 저장 ***
+    // *****************************************************************
+    let storeDifferences = [];
+    function readFileAndCompareWithOriginal(majorName,dataObject){
+        const rawData = fs.readFileSync(`./Backend/compare_list/${majorName}.json`,"utf8");
+        const oldContent = JSON.parse(rawData);
+        return compareTwoArrays(dataObject.url, oldContent.url);
+    }
+    storeDifferences.industSec = readFileAndCompareWithOriginal("industSec",new_industSec);
+    storeDifferences.software = readFileAndCompareWithOriginal("software",new_software);
+    storeDifferences.CAUnotice = readFileAndCompareWithOriginal("CAUnotice",new_CAUnotice);
+    storeDifferences.integEngineering = readFileAndCompareWithOriginal("integEngineering",new_integEngineering);
+    storeDifferences.korean = readFileAndCompareWithOriginal("korean",new_korean);
 
-    fs.readFile("./Backend/compare_list/industSec.json", function read(err, rawData) {
-        if (err) {throw err;}
-        const oldContent = JSON.parse(rawData);
-        industSecUpdates = compareTwoArrays(new_industSec.url, oldContent.url);
-    });
-    fs.readFile("./Backend/compare_list/software.json", function read(err, rawData) {
-        if (err) {throw err;}
-        const oldContent = JSON.parse(rawData);
-        softwareUpdates = compareTwoArrays(new_software.url, oldContent.url);
-    });
-    fs.readFile("./Backend/compare_list/CAUnotice.json", function read(err, rawData) {
-        if (err) {throw err;}
-        const oldContent = JSON.parse(rawData);
-        CAUnoticeUpdates = compareTwoArrays(new_CAUnotice.url, oldContent.url);
-    });
-    fs.readFile("./Backend/compare_list/integEngineering.json", function read(err, rawData) {
-        if (err) {throw err;}
-        const oldContent = JSON.parse(rawData);
-        integEngineeringUpdates = compareTwoArrays(new_integEngineering.url, oldContent.url);
-    });
-    fs.readFile("./Backend/compare_list/korean.json", function read(err, rawData) {
-        if (err) {throw err;}
-        const oldContent = JSON.parse(rawData);
-        koreanUpdates = compareTwoArrays(new_korean.url, oldContent.url);
-    });
-    // <Dummy>:
-    // fs.readFile("./Backend/compare_list/asdfasdf.json", function read(err, rawData) {
-    //     if (err) {throw err;}
-    //     const oldContent = JSON.parse(rawData);
-    //     asdfasdfUpdates = compareTwoArrays(new_asdfasdf.url, oldContent.url);
-    // });
+    
+    // ****************************************************
+    // *** 3. 변경 개수를 기반으로 실제 추가된 내용을 저장 ***
+    // ****************************************************
+    let updatedContentStorage = [];
+    function addURLsAndTitlesToStorage(majorName,dataObject,numberOfDifferences){
+        if(numberOfDifferences == 0) return;
+        let tempUrls = [];
+        let tempTitles = [];
+        for(let i=0;i<numberOfDifferences;i++){
+            tempUrls.push(dataObject.url[i]);
+            tempTitles.push(dataObject.title[i]);
+        }
+        console.log(tempUrls);
+        console.log(tempTitles);
+        updatedContentStorage[majorName] = {
+            url: tempUrls,
+            title: tempTitles
+        };
+    }
+    addURLsAndTitlesToStorage("industSec",new_industSec,storeDifferences.industSec);
+    addURLsAndTitlesToStorage("software",new_software,storeDifferences.software);
+    addURLsAndTitlesToStorage("CAUnotice",new_CAUnotice,storeDifferences.CAUnotice);
+    addURLsAndTitlesToStorage("integEngineering",new_integEngineering,storeDifferences.integEngineering);
+    addURLsAndTitlesToStorage("korean",new_korean,storeDifferences.korean);
+    
+
+    // ********************************************************************
+    // *** 4. 각 유저의 구독정보 확인 후 해당되는 게시글을 추가해 메일 전송 ***
+    // ********************************************************************
+    for(let i=0;i<nextIdNum;i++){
+
+    }
 }
+refresh();
+
+
+
 // 기존 목록의 파일들(json)을 만들고,
 // 기존 목록에서 긁어온 목록과 지금의 목록을 대조하고,
 // 결과값 리턴

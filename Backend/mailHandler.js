60e85fb6a5669604392172b6b1dff656058eb6da
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-07 16:10:47 +0900

    added test data and format

--- Backend/mailHandler.js
+++ Backend/mailHandler.js
@@ -1,16 +1,36 @@
 import {sendEmail, sendTemplateEmail} from "./sendEmail.js"
 import fs from "fs"
+import en2kr from "./name_en2kr.js"
 
-function mailHandler(){
+function mailHandler(recipientName, recipientEmail, data){
     // 수신자주소, 바디내용, 메일 제목을 받음
     // 바디 내용에는 어느 게시판을 구독했는지에 따라 추가적인 내용이 들어갈 수 있으므로
     // 앞부분 스트링을 bodyContent라는 string에 저장하고,
     // 그 뒷 부분에 추가할 부분을 <a href></a>로 추가하고(join 또는 concat 등등),
     // 제일 뒤에 </body>랑 </html> 등의 태그를 추가한다.
-    let bodyContent = fs.readFileSync("./Test2.html", "utf8");
+    
+
+    // 사용되는 변수: recipientName, majorName, url, title, unsubscribeUrl
+    // <a href="${url}">&#128204;${title}<br></a>
+    const numberOfMajors = Object.keys(data).length;
+    let updatedContent = '';
+    let numberOfUpdates = 0; // = 각 학과별 key의 개수 확인
+    for(let i=0;i<numberOfMajors;i++){
+      updatedContent.concat(`<h2>${data[i].majorName} 게시판:</h2>`);
+      numberOfUpdates = Object.keys(data[i]).length;
+      for(let j=0;j<numberOfUpdates;j++){
+        updatedContent = updatedContent.concat(`<a href="${data[i].url[j]}">&#128204;${data[i].title[j]}<br></a>`);
+      }
+    }
+    
+    
+    let bodyContent = `<!DOCTYPE html><html lang="ko"><title></title><head> <meta charset="UTF-8"></head><body> <h2>${recipientName}님,</h2> <p>${recipientName}님께서 구독하신 게시판의 새 공지&#128204;가 게시되었습니다!</p> ${updatedContent} <p>문의사항은 admin@caunotify.me 로 전달해주세요!&#128513;<br><br></p> <p>더 이상 공지를 받고 싶지 않으시다면, <a href="${unsubscribeUrl}">구독 해지</a>를 눌러 해지해주세요!</p> </body></html>`;
+    // let bodyContent = fs.readFileSync("./Test.html", "utf8");
+    
+    
     // bodyContent = "TestBodyContent";
     // params : recipientEmail, bodyContent, mailTitle
-    sendEmail("na_sanghyun@naver.com",bodyContent,"MailTitle")
+    sendEmail(recipientEmail, bodyContent,`${recipientName}님 새 공지가 게시되었습니다`)
     .then(
         function(data) {
           console.log(data);
@@ -20,4 +40,33 @@ function mailHandler(){
           console.error(err, err.stack);
         }); 
 }
-mailHandler();
+
+let testData = [
+  {
+    majorName: "산업보안학과",
+    url:[
+      "this is industSec URL one",
+      "this is industSec URL two"
+    ],
+    title:[
+      "this is industSec title one",
+      "this is industSec title two"
+    ]
+  },
+  {
+    majorName: "소프트웨어학부",
+    url:[
+      "this is software URL one",
+      "this is software URL two"
+    ],
+    title:[
+      "this is software title one",
+      "this is software title two"
+    ]
+  
+  }
+];
+
+// recipientName, recipientEmail, majorName, data, numberOfUpdates
+mailHandler("나상현","na_sanghyun@naver.com",testData);
+// console.log(testData);

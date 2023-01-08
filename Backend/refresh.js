51f2bfe123354b2b68b1ea4490df59f3af714c26
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-08 20:34:08 +0900

    Update refresh.js

--- Backend/refresh.js
+++ Backend/refresh.js
@@ -141,7 +141,9 @@ export async function refresh(nextIdNum){
     }
 
     console.log("storeDifferences.CAUnotice: " + storeDifferences.CAUnotice);
-    console.log("updatedContentStorage.CAUnotice: " + updatedContentStorage.CAUnotice);
+    console.log("storeDifferences.mechEngineering: " + storeDifferences.mechEngineering);
+    // updatedContentStorage.CAUnotice
+    console.log(updatedContentStorage.CAUnotice);
 
     // ********************************************
     // *** 5. 변경 사항이 있었던 게시판들은 초기화 ***

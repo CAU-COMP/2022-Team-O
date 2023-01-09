42b8c7eebcbce16c1f0c05a22a0f4c3e7bae2a50
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-09 22:09:01 +0900

    updated html

--- Backend/tempCodeRunnerFile.js
+++ Backend/tempCodeRunnerFile.js
@@ -1,9 +1,2 @@
-hile(1){
-//     let len = __dirname.length;
-//     console.log(len);
-//     let lastChar = len-1;
-//     if(lastChar != "\\"){
-//         __dirname = __dirname.substring(0, lastChar-1);
-//     }
-//     else break;
-// }
+me}:`);
+    // console.log(oldContent
--- Backend/test.js
+++ Backend/test.js
@@ -1,19 +1,37 @@
+import fs from "fs";
+import { compareTwoArrays } from "./compare.js"
 import path from 'path';
 import { fileURLToPath } from 'url';
+import crawlEnglish from "./crawlers/url_scraper_English.js";
 
 const __filename = fileURLToPath(import.meta.url);
-let __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-console.log(`OLD: ${__dirname}`);
-while(1){
-    let len = __dirname.length;
-    let lastChar = len-1;
-    if(__dirname[lastChar] != "\\"){
-        __dirname = __dirname.slice(0,lastChar);
-    }
-    else{
-        __dirname = __dirname.slice(0,lastChar);
-        break;
-    }
+const new_english = await crawlEnglish("url");
+console.log("english loaded");
+let storeDifferences = [];
+function readFileAndCompareWithOriginal(majorName,dataObject){
+    const rawData = fs.readFileSync(path.join(__dirname, 'compare_list', `${majorName}.json`),"utf8")
+    const oldContent = JSON.parse(rawData);
+    // console.log(`${majorName}:`);
+    // console.log(oldContent);
+    return compareTwoArrays(dataObject.url, oldContent.url);
 }
-console.log(`NEW: ${__dirname}`);
+
+storeDifferences.english = readFileAndCompareWithOriginal("english",new_english);
+console.log(storeDifferences.english[0]);
+
+if(storeDifferences.english != undefined && storeDifferences.english != 0){
+    let englishObject = {
+        url: new_english.url,
+        title: new_english.title
+    };
+    // englishObject = englishObject.push(new_english.url);
+    // console.log(`before : ${englishObject}`);
+    // englishObject = englishObject.push(new_english.title);
+    // console.log(`after : ${englishObject}`);
+    console.log(englishObject);
+    // fs.writeFile(path.join(__dirname, 'compare_list', 'english.json'), JSON.stringify(englishObject), (err) => {
+    //     if(err){console.log(err);}
+    //     else {console.log("english updated successfully");}});
+}
